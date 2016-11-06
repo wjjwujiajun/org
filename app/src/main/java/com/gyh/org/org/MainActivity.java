8b7a805218be3df15e98b1680073ed144222e716
guyahui <374211103@qq.com>
2016-11-06 15:17:16 +0800

    modify

--- app/src/main/java/com/gyh/org/org/MainActivity.java
+++ app/src/main/java/com/gyh/org/org/MainActivity.java
@@ -1,16 +1,37 @@
 package com.gyh.org.org;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-/**
- * sfasfasfasfas
- */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AsyncTask<Void, Void, Void>(){
+
+            @Override
+            protected void onPreExecute() {
+
+            }
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+
+            }
+        }.execute();
+
+
+        Toast.makeText(MainActivity.this, "sdfsdfsdfs", Toast.LENGTH_SHORT).show();
+
     }
 }
